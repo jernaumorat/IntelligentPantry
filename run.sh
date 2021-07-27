b3a815439257ab6caefcf780a434d1561836114a
Nathaniel Munk <nathaniel@munk.com.au>
2021-07-27 23:55:35 +1000

    config should actually be in database, and src should have a more descriptive name

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
 
-export FLASK_APP=src/app
+export FLASK_APP=flaskserv
 export FLASK_ENV=development
-export FLASK_RUN_PORT=`cat ./src/config.json | jq '.network.port'`
 
 python3 -m flask run
