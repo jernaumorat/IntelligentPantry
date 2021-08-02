8be5ac2e5dc3ff91d61c5920242ed7400f9946bd
Nathaniel Munk <nathaniel@munk.com.au>
2021-08-02 19:54:53 +1000

    register blueprints, fix paths/methods

--- pantryflask/__init__.py
+++ pantryflask/__init__.py
@@ -4,10 +4,31 @@ from flask import Flask, jsonify
 from zeroconf import ServiceInfo, Zeroconf
 
 from pantryflask.auth import token_auth
+from pantryflask.pantry_api import bp as pantry_bp
+from pantryflask.robot_api import bp as robot_bp
 
-app = Flask(__name__)
+def app_factory():
+    app = Flask(__name__)
+    app.register_blueprint(pantry_bp)
+    app.register_blueprint(robot_bp)
 
-@app.route('/')
-@token_auth.login_required
-def get_root():
-    return jsonify({'test': 'test'})
+    @app.route('/')
+    @token_auth.login_required
+    def get_root():
+        return jsonify({'test': 'test'})
+
+    @app.route('/cert', methods=['GET'])
+    def get_cert():
+        pass
+
+    @app.route('/pair', methods=['GET'])
+    def pair_device():
+        pass
+
+    @app.route('/pair', methods=['POST'])
+    @token_auth.login_required
+    def generate_pairing_codes():
+        pass
+
+    return app
+
--- pantryflask/pantry_api.py
+++ pantryflask/pantry_api.py
@@ -1,34 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from markupsafe import escape
 
-bp = Blueprint('pantry', __name__, url_prefix='pantry')
+bp = Blueprint('pantry', __name__, url_prefix='/pantry')
 
-@bp.route('/', methods=('GET'))
+@bp.route('/', methods=['GET'])
 def get_items():
+    return jsonify({'pantry': 'pantry'})
+
+@bp.route('/knownitems/', methods=['GET'])
+def get_allitems():
     pass
 
-@bp.route('/knownitems/', methods=('GET'))
-def get_():
-    pass
-
-@bp.route('/', methods=('POST'))
+@bp.route('/', methods=['POST'])
 def add_item():
     pass
 
-@bp.route('/knownitems/<int:itemID>', methods=('GET'))
-@bp.route('/<int:itemID>', methods=('GET'))
+@bp.route('/knownitems/<int:itemID>', methods=['GET'])
+@bp.route('/<int:itemID>', methods=['GET'])
 def get_item(itemID):
     pass
 
-@bp.route('/knownitems/<int:itemID>', methods=('PUT'))
-@bp.route('/<int:itemID>', methods=('PUT'))
+@bp.route('/knownitems/<int:itemID>', methods=['PUT'])
+@bp.route('/<int:itemID>', methods=['PUT'])
 def update_item(itemID):
     pass
 
-@bp.route('/<int:itemID>', methods=('DELETE'))
+@bp.route('/<int:itemID>', methods=['DELETE'])
 def delete_item(itemID):
     pass
 
-@bp.route('/knownitems/<int:itemID>', methods=('DELETE'))
+@bp.route('/knownitems/<int:itemID>', methods=['DELETE'])
 def delete_item_perm(itemID):
     pass
