e9bbb0e4173d24c9a820cfcbf6691c64586f557f
Blake Edwards <blakepotato@hotmail.com>
2021-09-20 20:50:09 +1000

    pantry field X and Y - added 2 new parameters to the shell file and syntax info

--- add-item.sh
+++ add-item.sh
@@ -4,12 +4,18 @@ URL="http://localhost:5000/pantry/"
 LABEL=$1
 QUANT=$2
 IMAGE=$3
+X=$4
+Y=$5
 
 echo $LABEL
 echo $QUANT
 echo $IMAGE
+echo $X
+echo $Y
 
-PAYLOAD='{"label":"'$LABEL'","quantity":"'$QUANT'"}'
+PAYLOAD='{"label":"'$LABEL'","quantity":"'$QUANT'","item_x":"'$X'","item_y":"'$Y'"}'
 
 echo $PAYLOAD
 curl -vv -X POST -F "payload=$PAYLOAD" -F "image=@$IMAGE" $URL
+#add item command syntax: ./add-item.sh 'label' qty 'image.jpg' X Y
+# eg ./add-item.sh 'apples' 5 'apples.jpg' 5 'apples.jog' 10 25
