5453c38ff3265b7fa931e92b384c0ca24ed09d5d
Blake Edwards <blakepotato@hotmail.com>
2021-09-22 23:49:14 +1000

    Base Abstract Class

---
+++ Bot.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+class Bot(ABC):
+    @abstractmethod
+    def moveTo(self , x , y):
+        pass
+
+    # get robot camera image of pantry
+    @abstractmethod
+    def getImage(self):
+        pass
+    
+    # scan pantry
+    @abstractmethod
+    def scan(self):
+        pass
+    
+    # system state scan /idle date and time
+    @abstractmethod
+    def getStatus(self):
+        pass
+    
