715fee9e3efe2db083dbbdf1fb43cda8363ec5a8
Blake Edwards <blakepotato@hotmail.com>
2021-09-23 19:34:26 +1000

    Redefined - flask enpoints used instead of while loop - endpoint moveTo

--- MainStub.py
+++ MainStub.py
@@ -1,42 +1,27 @@
 import pygame
 import time
-
+from flask import Blueprint, jsonify, request, make_response
+from json import loads
+from flask import Flask
+from Bot import Bot
 global BLUE, BLACK
 
 from PyBot import PyBot
 
+app = Flask(__name__)
 
-pygame.init()
+# endpoint to move bot to x y point
+# payload = {"x":int,"y":int}
+@app.route('/moveTo',methods=['POST'])
+def index():
+    payload = loads(request.form.get('payload'))
+    x = payload['x']
+    y = payload['y']
+    bot.moveTo(int(x), int(payload['y']))
+    return "",200
 
-active = True
 
-bot = PyBot(100, 100)
-bot.draw()
-print ("a = left\nd = right\nw = up\ns = down\nq or e = tilt\nr or f = forward/back")
-while active:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            active = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:         # up down left right
-                    bot.up()
-            if event.key == pygame.K_s:                
-                    bot.down()
-            if event.key == pygame.K_a:  
-                    bot.left()
-            if event.key == pygame.K_d:
-                    bot.right()
-
-            # if event.key == pygame.K_e:         # tilt up       tilt down
-            #         bot.tiltUp()
-            # if event.key == pygame.K_q:
-            #         bot.tiltDown()
-
-            # if event.key == pygame.K_r:         # Forward       Back
-            #         bot.forward()
-            # if event.key == pygame.K_f:
-            #         bot.back()
-
-            bot.draw()
-
-# Todo  create a Main function to initialise bot and run the flask server 
+if __name__ == "__main__":        
+    pygame.init()
+    bot = PyBot(100, 100)
+    app.run()
