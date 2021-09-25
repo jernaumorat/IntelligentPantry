452d765c229c00572fd5ab6ba2d82321e1c30b89
Blake Edwards <blakepotato@hotmail.com>
2021-09-25 18:06:33 +1000

    pybot getImage Func -uses self x / y to crop large img and saves to temp_img

--- PyBot.py
+++ PyBot.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 import time
+from PIL import Image
 from Bot import Bot
 # Default values
 
@@ -9,6 +10,8 @@ height = 800
 BLUE =      (0,   0, 255)
 BLACK =      (0,   0, 0)
 class PyBot(Bot):   
+    VIEW_WIDTH  = 600
+    VIEW_HIEGHT = 800    
     def __init__(self, x, y):
         self.win = pygame.display.set_mode((width, height))
         self.x = x
@@ -32,9 +35,21 @@ class PyBot(Bot):
     def erase(self):
         pygame.draw.circle(self.win, BLACK, (self.x,self.y),self.radius,2)
 
-    # Todo get robot camera image of pantry
+    # x and y are 0-100 meaning % of total width/hieght
     def getImage(self):
-        pass
+        # open image and crop to size
+        img = Image.open("pantry.jpg")
+        img_width  = img.size[0]
+        img_hieght = img.size[1]
+        
+        # the selectable screen  is full screen - the view
+        # we also need to know where the offset is base on the % of selectable screen
+        offset_x = (img_width - self.VIEW_WIDTH) /100 * self.x
+        offset_y = (img_hieght - self.VIEW_HIEGHT) /100 * self.y
+        cropped_img = img.crop((offset_x,offset_y,offset_x + self.VIEW_WIDTH, offset_y + self.VIEW_HIEGHT))        
+        img.close()
+        cropped_img.save("temp_img.jpg")
+        return
     
     # Todo scan pantry    
     def scan(self):
