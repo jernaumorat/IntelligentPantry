979ca1f1c0ec5e1ca5fb90667a409579fca5f64f
Nathaniel Munk <nathaniel@munk.com.au>
2021-10-04 09:31:16 +1100

    Tidy theme changes

--- components/SettingsScreen.tsx
+++ components/SettingsScreen.tsx
@@ -16,18 +16,15 @@ import {
     RefreshControl,
 } from 'react-native';
 
-// TODO: remove all NewAppScreen imports, then remove from package deps.
-import {
-    Colors,
-} from 'react-native/Libraries/NewAppScreen';
-import { Switch } from '../components';
+import { ThemeSwitch } from './ThemeSwitch';
+
 export const SettingsScreen = (props: any): JSX.Element => {
     return (
         <View style={{ flex: 1, paddingLeft: 30, paddingRight: 30, justifyContent: 'space-evenly' }}>
-             <View>
-             <Text style={{color: "green", textAlign :"right",width:"100%",alignSelf: 'flex-end' }} >Enable Dark Mode</Text>
-             <Switch/>
-             </View>
+            <View>
+                <Text style={{ color: "green", textAlign: "right", width: "100%", alignSelf: 'flex-end' }} >Enable Dark Mode</Text>
+                <ThemeSwitch />
+            </View>
             <Button color={'#1b7931'} title={"Scan"} onPress={() => { }} />
             <Button color={'#1b7931'} title={"Pair"} onPress={() => { }} />
             <Button color={'#1b7931'} title={"Dev Mode"} onPress={() => { }} />
--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,31 +1,31 @@
 import * as React from 'react';
 import { useState } from 'react';
-import {Switch as RNSwitch, Text, useColorScheme, View} from 'react-native';
+import { Switch as RNSwitch, Text, useColorScheme, View } from 'react-native';
 
 //react native Library used to send and recieve events https://www.npmjs.com/package/react-native-event-listeners
 import { EventRegister } from 'react-native-event-listeners'
 
 
 //create a toggle switch component provided by react native 
-export const Switch = (props: any): JSX.Element => {
-    
+export const ThemeSwitch = (props: any): JSX.Element => {
+
     // variable to hold the toggle state or value ie true/flase 
     const [isEnabled, setIsEnabled] = useState(false);
-    
+
     const toggleScheme = () => {
         //set switched value to isEnabled variable
         setIsEnabled(previousState => !previousState);
-         // emit signal(envoke changeTheme event) to change theme in the App.tsx with value of switch 
+        // emit signal(envoke changeTheme event) to change theme in the App.tsx with value of switch 
         EventRegister.emit('changeTheme', !isEnabled);
-        
+
     }
 
     return (
-       
+
         <RNSwitch
-        trackColor={{true: 'grey', false: 'black'}}
-        style={{ transform: [{ scaleX: 1 }, { scaleY: 1 }] }}
-        value={isEnabled} onValueChange={toggleScheme}/>
-        
+            trackColor={{ true: 'grey', false: 'black' }}
+            style={{ transform: [{ scaleX: 1 }, { scaleY: 1 }] }}
+            value={isEnabled} onValueChange={toggleScheme} />
+
     );
 }
